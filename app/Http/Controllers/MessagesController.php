aa22c2e3eaa6b02b35c9f37e9929b96d85d0dece
i339417 <tibor.seres@sap.com>
2017-08-02 14:36:02 +0200

    email verification

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -12,7 +12,7 @@ class MessagesController extends Controller
 
       $this->validate($request, [
         'name' => 'required',
-        'email' => 'required',
+        'email' => 'required|email',
         'message' => 'required'
 
       ]);
